7732b5ef5dadb40013e6359cc38ce9be0df7ac0a
fusikky <fusikky@gmail.com>
2015-07-03 00:30:10 +0900

    create post

--- railsBBS/app/controllers/posts_controller.rb
+++ railsBBS/app/controllers/posts_controller.rb
@@ -3,7 +3,23 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
 
+  def new
+    @post = Post.new
+  end
 
+  def create
+    @post = Post.new(post_params) # formからの値を取得
+    @post.save # DBに保存
+    redirect_to posts_path
+  end
+
+  private
+    def post_params
+      params[:post].permit(:title, :text) # form のtitleのみ許可
+    end
 
 end
