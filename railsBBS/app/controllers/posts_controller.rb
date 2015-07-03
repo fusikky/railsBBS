53be1c46c17b8cae31d9b2b698cc95a4614ce877
fusikky <fusikky@gmail.com>
2015-07-04 01:41:18 +0900

    add delete method

--- railsBBS/app/controllers/posts_controller.rb
+++ railsBBS/app/controllers/posts_controller.rb
@@ -34,6 +34,12 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to post
+  end
+
   private
     def post_params
       params[:post].permit(:title, :text) # form のtitleのみ許可
