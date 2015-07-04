1e421361f0ef2adcc87f6372603ddc6817024111
fusikky <fusikky@gmail.com>
2015-07-04 12:31:37 +0900

    add users

--- railsBBS/app/controllers/users_controller.rb
+++ railsBBS/app/controllers/users_controller.rb
@@ -1,2 +1,51 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params) # formからの値を取得
+
+    if @user.save # DBに保存
+      redirect_to users_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+    def user_params
+      params[:user].permit(:name)
+    end
+
+   def set_user
+      @user = User.find(params[:id])
+   end
+
 end
--- railsBBS/config/routes.rb
+++ railsBBS/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :posts
-  resources :user
+  resources :users
 
   root 'posts#index'
 
