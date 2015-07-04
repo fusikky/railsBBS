89b7f5fdbc949f8534d9c010324f5e50b23cb2db
fusikky <fusikky@gmail.com>
2015-07-04 11:34:01 +0900

    set before_action

--- railsBBS/app/controllers/posts_controller.rb
+++ railsBBS/app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 class PostsController < ApplicationController
+
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -22,11 +24,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id]) # formからの値を取得
     if @post.update(post_params)
       redirect_to posts_path
     else
@@ -35,7 +35,6 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to post
   end
@@ -45,4 +44,7 @@ class PostsController < ApplicationController
       params[:post].permit(:title, :text) # form のtitleのみ許可
     end
 
+    def set_post
+      @post = Post.find(params[:id])
+    end
 end
